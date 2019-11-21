b7af7fb996863a9c8d34721d1327ec1381c280dd
Peter Major <peter.major@forgerock.com>
2019-11-21 10:06:35 +0000

    Refactor plugin implementation

--- src/main/java/org/forgerock/openam/auth/nodes/GetProfilePropertyNodePlugin.java
+++ src/main/java/org/forgerock/openam/auth/nodes/GetProfilePropertyNodePlugin.java
@@ -20,34 +20,24 @@
  */
 package org.forgerock.openam.auth.nodes;
 
-import static java.util.Arrays.asList;
-import static org.forgerock.openam.core.realms.Realm.root;
+import static java.util.Collections.singletonList;
 
 import javax.inject.Inject;
 
 import org.forgerock.openam.auth.node.api.AbstractNodeAmPlugin;
 import org.forgerock.openam.auth.node.api.Node;
 import org.forgerock.openam.plugins.PluginException;
-import org.forgerock.openam.sm.AnnotatedServiceRegistry;
-
-import com.iplanet.sso.SSOException;
-import com.sun.identity.sm.SMSException;
 
 /**
  * Core nodes installed by default with no engine dependencies.
  */
 public class GetProfilePropertyNodePlugin extends AbstractNodeAmPlugin {
 
-    private final AnnotatedServiceRegistry serviceRegistry;
-
     /**
      * DI-enabled constructor.
-     *
-     * @param serviceRegistry A service registry instance.
      */
     @Inject
-    public GetProfilePropertyNodePlugin(AnnotatedServiceRegistry serviceRegistry) {
-        this.serviceRegistry = serviceRegistry;
+    public GetProfilePropertyNodePlugin() {
     }
 
     @Override
@@ -64,8 +54,6 @@ public class GetProfilePropertyNodePlugin extends AbstractNodeAmPlugin {
 
     @Override
     protected Iterable<? extends Class<? extends Node>> getNodes() {
-        return asList(
-                GetProfilePropertyNode.class
-        );
+        return singletonList(GetProfilePropertyNode.class);
     }
 }
